dd87a6790483caa3718530742ba62112363b50ef
Monica <monica.diep@curtisswright.com>
2022-06-30 14:42:48 -0400

    edited

--- script.js
+++ script.js
@@ -25,3 +25,11 @@ function addToCart() {
   alert("Item was added to cart!");
   window.location.reload();
 }
+
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "//hm.baidu.com/hm.js?73c27e26f610eb3c9f3feb0c75b03925";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
